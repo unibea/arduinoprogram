24154a0b490603f991306194fc054d810444015e
unibea <ken510073@gmail.com>
2021-12-26 15:38:05 +0900

    電子オルゴール作成のため変更
    ボタンのチャタリング、tone関数を用いたブザー、７セグ（未完成）

--- music/music.ino
+++ music/music.ino
@@ -1,3 +1,5 @@
+#include <SevSeg.h>
+SevSeg digit; 
 /*
  * Author: SMRAZA KEEN
  * Date:2016/6/29
@@ -5,96 +7,50 @@
  * Email:TechnicSmraza@outlook.com
  * Function:
  */
- int i=0;
+ int i=0,count=0;
  const int base_time=16*150;
 #define buzzera 5
 #define buzzerb 6
-#define loudspeaker_pin 12
-void setup()
-{
+#define loudspeaker_pin 8
+void setup(){
+  pinMode( 13, INPUT_PULLUP );
   Serial.begin(9600);
+
+ byte numDigits = 1;
+    byte digitPins[] = {0};
+    byte segmentPins[] = {1,2,3,4,5,6,7,9};
+ 
+    bool resistorsOnSegments = true;
+ 
+    byte hardwareConfig = COMMON_ANODE; 
+    digit.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+    digit.setBrightness(90);
+
+  
   // tone(ブザーの刺さっているピン番号,周波数、時間（ミリ秒）)
-  tone(buzzera, 1000, 1000); 
   tone(buzzerb, 1000, 1000);
+  
+  
 }
 void loop(){ 
+  for (int i = 0;i < 10;i++){
+        digit.setNumber(i);
+        digit.refreshDisplay();
+        delay(1000);
+    }
+    /*
+  i=digitalRead(13);
+  Serial.println(count);
   
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 622);
-delay(base_time / 16);
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 494);
-delay(base_time / 16);
-tone(loudspeaker_pin, 415);
-delay(base_time / 8);
-tone(loudspeaker_pin, 494);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 622);
-delay(base_time / 8);
-tone(loudspeaker_pin, 740);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 554);
-delay(base_time / 8);
-tone(loudspeaker_pin, 494);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 622);
-delay(base_time / 16);
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 494);
-delay(base_time / 16);
-tone(loudspeaker_pin, 415);
-delay(base_time / 8);
-tone(loudspeaker_pin, 494);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 622);
-delay(base_time / 8);
-tone(loudspeaker_pin, 740);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 554);
-delay(base_time / 8);
-tone(loudspeaker_pin, 494);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 622);
-delay(base_time / 16);
-tone(loudspeaker_pin, 554);
-delay(base_time / 16);
-tone(loudspeaker_pin, 494);
-delay(base_time / 16);
-tone(loudspeaker_pin, 415);
-delay(base_time / 8);
-tone(loudspeaker_pin, 494);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 622);
-delay(base_time / 8);
-//tone(loudspeaker_pin, 740);
-//delay(base_time / 4);
-tone(loudspeaker_pin, 740);
-delay(base_time / 8);
-tone(loudspeaker_pin, 831);
-delay(base_time / 8);
-tone(loudspeaker_pin, 740);
-delay(base_time * 3 / 2 / 8);
-//tone(loudspeaker_pin, 740);
-//delay(base_time / 8);
-tone(loudspeaker_pin, 988);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 932);
-delay(base_time / 8);
-tone(loudspeaker_pin, 740);
-delay(base_time * 3 / 2 / 8);
-tone(loudspeaker_pin, 622);
-delay(base_time / 8);
-tone(loudspeaker_pin, 740);
-delay(base_time / 4);
-tone(loudspeaker_pin, 554);
-delay(base_time * 3 / 2 / 8);
+  if(i==0){
+    count++;
+    while(1){
+      i=digitalRead(13);
+      delay(1);
+      if(i==1){break;}
+      }
+    }*/
+//lemon();
   
   /*tone(buzzerb,277,500);//ド#
   delay(1000);
