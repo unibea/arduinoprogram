7129c3c57fc660f638626d75e1506cc4fc73b938
unibea <ken510073@gmail.com>
2022-02-07 02:03:48 +0900

    サーボモーターのテストでの変更

--- serbo_test/serbo_test.ino
+++ serbo_test/serbo_test.ino
@@ -1,4 +1,4 @@
-#define pulse 1700 　//1150 1700
+#define pulse 1700 
 void setup(){
   //パルス出力ピンの設定
   //（デジタル出力なのでどのピンでも可）
@@ -12,10 +12,10 @@ void setup(){
 }
 int i=-50;
 void servo(int a){
-  digitalWrite(30,HIGH); //digitalWrite(1,HIGH);
-  delayMicroseconds(); 
-  digitalWrite(30,LOW); //digitalWrite(1,LOW);
-  delayMicroseconds(20000-pulse);
+  digitalWrite(40,HIGH); //digitalWrite(1,HIGH);
+  delayMicroseconds(a); 
+  digitalWrite(40,LOW); //digitalWrite(1,LOW);
+  delayMicroseconds(20000-a);
   }
 void loop(){
   /*//可変抵抗器の読み込み値を２倍にする（振幅値：約2000）
@@ -32,7 +32,7 @@ void loop(){
   delayMicroseconds(500);
   */
   while(i<0){
-   servo(1350);
+   servo(820);//820: 閉まる1300:開く
    i=i+1;
   }
     
